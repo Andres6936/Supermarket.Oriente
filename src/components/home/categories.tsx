63791c0636e12bfbb03febdfea5c35cef291dbd4
Andres6936 <andres6936@gmail.com>
2025-04-12 23:03:21 -0500

    Refactor category layout for improved spacing and alignment
    Added padding to the main container and adjusted child div styling to enhance layout consistency and readability. Wrapped category titles and item counts in a container for better alignment. These changes improve the visual structure and responsiveness of the category section.

--- src/components/home/categories.tsx
+++ src/components/home/categories.tsx
@@ -1,7 +1,7 @@
 export const Categories = () => {
     return (
         <section
-            className="flex flex-row gap-4 items-center justify-around pt-3 mx-1 text-center bg-white border border-top-0 overflow-auto">
+            className="flex flex-row gap-4 items-center justify-around pt-3 mx-1 text-center bg-white border border-top-0 overflow-auto px-3">
             {[
                 {src: "./img/item/8.jpg", title: "Breakfast & Dairy", items: "857 Items"},
                 {src: "./img/item/3.jpg", title: "Beverages", items: "65 Items"},
@@ -9,7 +9,7 @@ export const Categories = () => {
                 {src: "./img/item/9.jpg", title: "Biscuits, Snacks & Chocolates", items: "48 Items"},
                 {src: "./img/item/11.jpg", title: "Pet Care", items: "568 Items"}
             ].map((category, index) => (
-                <div className="flex flex-col items-center" key={index}>
+                <div className="flex flex-1 flex-col justify-between items-center" key={index}>
                     <div className="w-[64px] aspect-square">
                         <img
                             src={category.src}
@@ -19,8 +19,10 @@ export const Categories = () => {
                             width="80"
                         />
                     </div>
-                    <h6 className="h6 fw-bold small">{category.title}</h6>
-                    <p className="text-muted small">{category.items}</p>
+                    <div>
+                        <h6 className="h6 fw-bold small">{category.title}</h6>
+                        <p className="text-muted small">{category.items}</p>
+                    </div>
                 </div>
             ))}
         </section>
