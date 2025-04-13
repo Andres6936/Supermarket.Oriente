0a9ac21587ffc0f941dbc4ddd40428a61b2a31b3
Andres6936 <andres6936@gmail.com>
2025-04-12 22:55:51 -0500

    Update banner layout to use flexbox for responsiveness
    Replaced Bootstrap grid classes with Tailwind flexbox classes to improve layout structure. The updated implementation ensures better responsiveness, especially for mobile and tablet views.

--- src/components/home/banners.tsx
+++ src/components/home/banners.tsx
@@ -1,13 +1,13 @@
 export const Banners = () => {
     return (
-        <section className="row px-4 my-5">
+        <section className="flex flex-col md:flex-row px-4 my-5">
             <img
-                className="col-12 col-sm-6 img-fluid mx-0 px-0"
+                className="w-full mx-0 px-0"
                 alt="Off Products"
                 src="./img/slides/1(2).jpg"
             />
             <img
-                className="col-12 col-sm-6 img-fluid mx-0 px-0"
+                className="w-full mx-0 px-0"
                 alt="Off Products"
                 src="./img/slides/2(2).jpg"
             />
