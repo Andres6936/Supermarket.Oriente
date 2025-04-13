fbb6ae1a3af1227ed26191f853a0888ca6cdafe4
Andres6936 <andres6936@gmail.com>
2025-04-12 23:01:02 -0500

    Improve category layout and scrolling behavior
    Added gap between category items and enabled horizontal scrolling with "overflow-auto". Adjusted image styling to be more consistent by wrapping images in a fixed square container and removing redundant flex properties. These changes enhance both visual alignment and usability.

--- src/components/home/categories.tsx
+++ src/components/home/categories.tsx
@@ -1,7 +1,7 @@
 export const Categories = () => {
     return (
         <section
-            className="flex flex-row items-center justify-around pt-3 mx-1 text-center bg-white border border-top-0">
+            className="flex flex-row gap-4 items-center justify-around pt-3 mx-1 text-center bg-white border border-top-0 overflow-auto">
             {[
                 {src: "./img/item/8.jpg", title: "Breakfast & Dairy", items: "857 Items"},
                 {src: "./img/item/3.jpg", title: "Beverages", items: "65 Items"},
@@ -9,14 +9,16 @@ export const Categories = () => {
                 {src: "./img/item/9.jpg", title: "Biscuits, Snacks & Chocolates", items: "48 Items"},
                 {src: "./img/item/11.jpg", title: "Pet Care", items: "568 Items"}
             ].map((category, index) => (
-                <div className="flex flex-col items-center justify-center" key={index}>
-                    <img
-                        src={category.src}
-                        alt="Item"
-                        className="img-thumbnail rounded-circle mb-2"
-                        height="80"
-                        width="80"
-                    />
+                <div className="flex flex-col items-center" key={index}>
+                    <div className="w-[64px] aspect-square">
+                        <img
+                            src={category.src}
+                            alt="Item"
+                            className="border w-full aspect-square rounded-circle mb-2"
+                            height="80"
+                            width="80"
+                        />
+                    </div>
                     <h6 className="h6 fw-bold small">{category.title}</h6>
                     <p className="text-muted small">{category.items}</p>
                 </div>
